be9aadccca861f274d129ef4cfbc72ed51ed9bcd
Andrew Klychkov <aklychko@redhat.com>
2021-06-29 16:18:18 +0200

    Fix

--- plugins/module_utils/user.py
+++ plugins/module_utils/user.py
@@ -1,3 +1,6 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 # This code is part of Ansible, but is an independent component.
 # This particular file snippet, and this file snippet only, is BSD licensed.
 # Modules you write using this snippet, which is embedded dynamically by Ansible

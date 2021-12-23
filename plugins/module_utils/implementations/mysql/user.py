146681c84989cf3ff3af02cbbf1e1c735e649c24
Andrew Klychkov <aklychko@redhat.com>
2021-12-23 14:45:21 +0300

    Prepare for distutils.version being removed in Python 3.12 (#267)
    * Prepare for distutils.version being removed in Python 3.12
    
    * Update plugins/module_utils/version.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    (cherry picked from commit d9e12b85ad8f73b83de6a1a039ae2fe37e021225)

--- plugins/module_utils/implementations/mysql/user.py
+++ plugins/module_utils/implementations/mysql/user.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from distutils.version import LooseVersion
+from ansible_collections.community.mysql.plugins.module_utils.version import LooseVersion
 from ansible_collections.community.mysql.plugins.module_utils.mysql import get_server_version
 
 

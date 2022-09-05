fec19e73d2e9aa37fade2db05e87c37e86f93f89
Gabriel PREDA <gabriel@e-radical.ro>
2022-09-05 12:18:25 +0300

    * add `socket` option suggestion in documentation

--- plugins/doc_fragments/mysql.py
+++ plugins/doc_fragments/mysql.py
@@ -81,6 +81,9 @@ requirements:
 notes:
    - "To avoid the C(Please explicitly state intended protocol) error, use the I(login_unix_socket) argument,
      for example, C(login_unix_socket: /run/mysqld/mysqld.sock)."
+   - Alternatively, to avoid using I(login_unix_socket) argument on each invocation you can specify the socket path 
+     using the `socket` option in your MySQL config file (usually C(~/.my.cnf)) on the destination host, for 
+     example C(socket=/var/lib/mysql/mysql.sock).
    - Requires the PyMySQL (Python 2.7 and Python 3.X) or MySQL-python (Python 2.X) package installed on the remote host.
      The Python package may be installed with apt-get install python-pymysql (Ubuntu; see M(ansible.builtin.apt)) or
      yum install python2-PyMySQL (RHEL/CentOS/Fedora; see M(ansible.builtin.yum)). You can also use dnf install python2-PyMySQL

e8d7de4a64e33921e76d56b7bd2e72272e81655f
Laurent Indermuehle <laurent.indermuehle@pm.me>
2023-01-09 19:50:25 +0100

    Cut Ansible since we can't change what the user have on his computer

--- run_all_tests.py
+++ run_all_tests.py
@@ -36,16 +36,12 @@ def is_exclude(exclude_list, test_suite):
             if excl.get('db_engine_version') == test_suite[0]:
                 match += 1
 
-        if 'ansible' in excl:
-            if excl.get('ansible') == test_suite[1]:
-                match += 1
-
         if 'python' in excl:
-            if excl.get('python') == test_suite[2]:
+            if excl.get('python') == test_suite[1]:
                 match += 1
 
         if 'connector' in excl:
-            if excl.get('connector') == test_suite[3]:
+            if excl.get('connector') == test_suite[2]:
                 match += 1
 
         if match > 1:
@@ -61,11 +57,10 @@ def main():
     matrix = []
     exclude_list = tests_matrix_yaml.get('exclude')
     for db_engine in tests_matrix_yaml.get('db_engine_version'):
-        for ansible in tests_matrix_yaml.get('ansible'):
-            for python in tests_matrix_yaml.get('python'):
-                for connector in tests_matrix_yaml.get('connector'):
-                    if not is_exclude(exclude_list, (db_engine, ansible, python, connector)):
-                        matrix.append((db_engine, ansible, python, connector))
+        for python in tests_matrix_yaml.get('python'):
+            for connector in tests_matrix_yaml.get('connector'):
+                if not is_exclude(exclude_list, (db_engine, python, connector)):
+                    matrix.append((db_engine, python, connector))
 
     print(len(matrix))
 

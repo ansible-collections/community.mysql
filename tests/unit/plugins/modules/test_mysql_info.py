65e9a4e9d64d25402cae289fb6caf4b53675a1ea
Laurent Indermuehle <laurent.indermuehle@pm.me>
2024-07-19 15:48:06 +0200

    Fix unit tests

--- tests/unit/plugins/modules/test_mysql_info.py
+++ tests/unit/plugins/modules/test_mysql_info.py
@@ -14,15 +14,15 @@ from ansible_collections.community.mysql.plugins.modules.mysql_info import MySQL
 
 
 @pytest.mark.parametrize(
-    'suffix,cursor_output,server_implementation,user_implementation',
+    'suffix,cursor_output,server_implementation,server_version,user_implementation',
     [
-        ('mysql', '5.5.1-mysql', 'mysql', 'mysql'),
-        ('log', '5.7.31-log', 'mysql', 'mysql'),
-        ('mariadb', '10.5.0-mariadb', 'mariadb', 'mariadb'),
-        ('', '8.0.22', 'mysql', 'mysql'),
+        ('mysql', '5.5.1-mysql', 'mysql', '5.5.1', 'mysql'),
+        ('log', '5.7.31-log', 'mysql', '5.7.31', 'mysql'),
+        ('mariadb', '10.5.0-mariadb', 'mariadb', '10.5.0', 'mariadb'),
+        ('', '8.0.22', 'mysql', '8.0.22', 'mysql'),
     ]
 )
-def test_get_info_suffix(suffix, cursor_output, server_implementation, user_implementation):
+def test_get_info_suffix(suffix, cursor_output, server_implementation, server_version, user_implementation):
     def __cursor_return_value(input_parameter):
         if input_parameter == "SHOW GLOBAL VARIABLES":
             cursor.fetchall.return_value = [{"Variable_name": "version", "Value": cursor_output}]
@@ -32,6 +32,6 @@ def test_get_info_suffix(suffix, cursor_output, server_implementation, user_impl
     cursor = MagicMock()
     cursor.execute.side_effect = __cursor_return_value
 
-    info = MySQL_Info(MagicMock(), cursor, server_implementation, user_implementation)
+    info = MySQL_Info(MagicMock(), cursor, server_implementation, server_version, user_implementation)
 
     assert info.get_info([], [], False)['version']['suffix'] == suffix
